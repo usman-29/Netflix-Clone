9d86cfb566f44286b35ed20e39d7fbf886e6cb7f
Usman Saeed <uusaeed749@gmail.com>
2025-01-01 21:04:29 +0500

    home page done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,10 +24,11 @@ const Navbar = () => {
                 <img src={search_icon} alt='' className='icons' />
                 <p>Children</p>
                 <img src={bell_icon} alt='' className='icons' />
-                <div className='navbar-profile'></div>
-                <img src={profile_img} alt='' className='profile' />
-                <img src={caret_icon} alt='' className='icons' />
-
+                <div className='navbar-profile'>
+                    <img src={profile_img} alt='' className='profile' />
+                    <img src={caret_icon} alt='' className='icons' />
+                    <div className="dropdown">Sign Out of of Netflix</div>
+                </div>
             </div>
         </div>
     )
