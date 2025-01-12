bffdc92db86cfb5c6c6689986c1ed5f2d6329d61
Usman Saeed <uusaeed749@gmail.com>
2025-01-12 23:38:09 +0500

    frontend done

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './TitleCards.css';
 import cards_data from '../../assets/cards/Cards_data';
+import { Link } from 'react-router-dom';
 
 const TitleCards = ({ title, category }) => {
+    const [apiData, setApiData] = useState([]);
     const cardsRef = useRef();
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNThlYTUzZWVmNWJiZjRlZjJkYjVkZjYxNjc0MTgyYiIsIm5iZiI6MTczNjY5Nzk5My4zNzYsInN1YiI6IjY3ODNlODg5YWJhYmJiYTA0MGJiNGY4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cI_cvrGHv2sSWBWapwdFXqmcUfPq4WpjC94xHf_cTiw'
+        }
+    };
+
+
 
     const handleWheel = (event) => {
         event.preventDefault();
@@ -11,6 +23,10 @@ const TitleCards = ({ title, category }) => {
     }
 
     useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+            .then(res => res.json())
+            .then(res => setApiData(res.results))
+            .catch(err => console.error(err));
         cardsRef.current.addEventListener('wheel', handleWheel)
     }, [])
 
@@ -18,12 +34,12 @@ const TitleCards = ({ title, category }) => {
         <div className='title-cards'>
             <h2>{title ? title : 'Popular on Netflix'}</h2>
             <div className='card-list' ref={cardsRef}>
-                {cards_data.map((card, index) => {
+                {apiData.map((card, index) => {
                     return (
-                        <div key={index} className='card'>
-                            <img src={card.image} alt='' />
-                            <p>{card.name}</p>
-                        </div>
+                        <Link to={`/player/${card.id}`} key={index} className='card'>
+                            <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt='' />
+                            <p>{card.original_title}</p>
+                        </Link>
                     )
                 }
                 )}
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Player.css';
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Player = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [apiData, setApiData] = useState({
+        name: "",
+        key: "",
+        published_at: "",
+        typeof: ""
+    });
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNThlYTUzZWVmNWJiZjRlZjJkYjVkZjYxNjc0MTgyYiIsIm5iZiI6MTczNjY5Nzk5My4zNzYsInN1YiI6IjY3ODNlODg5YWJhYmJiYTA0MGJiNGY4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cI_cvrGHv2sSWBWapwdFXqmcUfPq4WpjC94xHf_cTiw'
+        }
+    };
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+            .then(res => res.json())
+            .then(res => setApiData(res.results[0]))
+            .catch(err => console.error(err))
+    }, [])
+
+    if (!apiData) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className='player'>
-
+            <img src={back_arrow_icon} onAbort={() => { navigate(-2) }} />
+            <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
+            <div className='player-info'>
+                <p>{apiData.published_at.slice(0, 10)}</p>
+                <p>{apiData.name}</p>
+                <p>{apiData.typeof}</p>
+            </div>
         </div>
     )
 }
